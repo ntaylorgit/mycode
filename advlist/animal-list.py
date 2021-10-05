59ee5e420e844926da25da1b275cdc4bed3738ab
Nikki Taylor <ntaylorgit@users.noreply.github.com>
2021-10-05 12:44:53 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - simple list - challenge"""
+
+def main():
+    # create a list called list1
+    list1 = "Fox Fly Ant Bee Cod Cat Dog Yak Cow Hen Koi Hog Jay Kit"
+
+    # display list1
+    print(list1)
+
+main()
